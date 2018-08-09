cc8717769a5c33480934d5e55bbacaa7758414cc
Pan Cheng <pchengeducation@hotmail.com>
2018-08-09 17:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ class Driver{
     this.startDate = new Date(startDate);
   }
 
-  yearsExperienceFromBeginningOf(){
-
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear() -1;
   }
 }
 class Route{
